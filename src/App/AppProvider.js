f8e393bd4d3b2fa80076893ac2e93b2fd2e3c16b
Andrew Smith <ajs3993@gmail.com>
2020-01-30 14:47:31 -0600

    add loading content

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 
+const cc = require('cryptocompare');
+
 export const AppContext = React.createContext();
 
 export class AppProvider extends React.Component{
@@ -14,6 +16,15 @@ export class AppProvider extends React.Component{
             confirmFavorites: this.confirmFavorites
         }
     }
+
+componentDidMount = () => {
+    this.fetchCoins();
+}
+
+fetchCoins = async () => {
+    let coinList = (await cc.coinList()).Data;
+    this.setState({coinList});
+}
 
     confirmFavorites= ()=>{
         this.setState({
--- src/App/index.js
+++ src/App/index.js
@@ -3,7 +3,8 @@ import './App.css';
 import AppLayout from './AppLayout';
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider';
-import Settings from '../Settings'
+import Settings from '../Settings';
+import Content from '../Shared/Content';
 
 
 
@@ -13,7 +14,9 @@ class App extends Component {
       <AppLayout>
         <AppProvider>
           <AppBar/>
-          <Settings/>
+          <Content>
+            <Settings/>
+          </Content>
         </AppProvider>
       </AppLayout>
     ); 
