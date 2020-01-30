e5199d72dfd833ed9dd948017c35ab9d765c6c0f
Andrew Smith <ajs3993@gmail.com>
2020-01-30 14:57:32 -0600

    map through coin list

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import {AppContext} from '../App/AppProvider';
+
+export const CoinGridStyled = styled.div`
+display: grid;
+`
+export default function (){
+    return <AppContext.Consumer>
+        {({coinList}) => <CoinGridStyled>
+        {Object.keys(coinList).map(coinKey => 
+        <div> {coinKey} </div>)}
+        </CoinGridStyled>}
+    </AppContext.Consumer>
+}
